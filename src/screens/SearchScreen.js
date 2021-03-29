09fe730ddab922aac9d7c3d00be3ef0a641974f5
Wayne Lee <waynel1996weiyang@gmail.com>
2021-03-29 16:02:14 -0700

    Implement the search bar component built in class before.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = ({navigation}) => {
+  const [term, setTerm] = useState('');
+
   return(
     <View>
+      <SearchBar
+        term={term} 
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => {}} />
       <TouchableOpacity onPress={() => {
         navigation.navigate('Detail')
       }}>
