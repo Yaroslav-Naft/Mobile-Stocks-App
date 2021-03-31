c9cebb62bbd99dcaef0b850562c240212d1cbf50
Gökay Abay <abaygokay@gmail.com>
2021-03-31 07:29:24 -0700

    conditionally render if user is not logged in

--- App.js
+++ App.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
-import LoginScreenNavigator from './src/components/navigation/LoginScreenNavigator';
-import BottomTabNavigator from './src/components/navigation/BottomTabNavigator';
-import MainPageLoginNavigator from './src/components/navigation/MainPageLoginNavigator';
+import React from "react"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
+import { StyleSheet } from "react-native"
+import LoginScreenNavigator from "./src/components/navigation/LoginScreenNavigator"
+import BottomTabNavigator from "./src/components/navigation/BottomTabNavigator"
+import MainPageLoginNavigator from "./src/components/navigation/MainPageLoginNavigator"
+import { useState } from "react/cjs/react.development"
+import { AuthProvider } from "./src/context/AuthProvider"
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 export default function App() {
+  const { user, setUser } = useContext(AuthContext)
+
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Root" component={BottomTabNavigator} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <AuthProvider>
+        <NavigationContainer>
+          {user ? (
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Root" component={BottomTabNavigator} />
+            </Stack.Navigator>
+          ) : (
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="Root" component={LoginScreenNavigator} />
+            </Stack.Navigator>
+          )}
+        </NavigationContainer>
+      </AuthProvider>
     </SafeAreaProvider>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
