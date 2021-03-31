f0fce08b2e4a7d587cca27c379e4b89fff426b58
Wayne Lee <waynel1996weiyang@gmail.com>
2021-03-31 12:16:12 -0700

    List my positions in the home page.

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react'
+import { StyleSheet, View, Text, SafeAreaView, FlatList } from 'react-native'
+import BoughtStockListItem from '../components/home/BoughtStockListItem'
 
 const HomeScreen = () => {
+  const placeholder = [ 
+    { stockName: "detail1", company: "example", boughtAmount: 100, marketPrice: 200, boughtPrice: 100 },
+    { stockName: "detail2", company: "example", boughtAmount: 200, marketPrice: 200, boughtPrice: 150 },
+    { stockName: "detail3", company: "example2", boughtAmount: 100, marketPrice: 100, boughtPrice: 150 }
+  ]
+
   return(
-    <View>
-      <Text>Account</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Position</Text>
+      <SafeAreaView>
+        <FlatList
+          keyExtractor={item => item.stockName}
+          data={placeholder}
+          renderItem={({ item }) => {
+            return (
+              <BoughtStockListItem item={item} />
+            )
+          }}
+        />
+      </SafeAreaView>
     </View>
   )
 }
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'uppercase'
+  }
+})
+
+export default HomeScreen
