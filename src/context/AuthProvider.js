336261c0a782160defda7fd04eca6d7520480ed9
Gökay Abay <abaygokay@gmail.com>
2021-03-31 07:27:16 -0700

    AuthProvider created

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState } from "react"
+import auth from "@react-native-firebase/auth"
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
