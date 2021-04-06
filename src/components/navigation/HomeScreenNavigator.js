c4e4e60ba5f80fcaab5e3e24df0563607c29f612
Wayne Lee <waynel1996weiyang@gmail.com>
2021-04-06 10:53:18 -0700

    Add detail screen to home page.

--- src/components/navigation/HomeScreenNavigator.js
+++ src/components/navigation/HomeScreenNavigator.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Button } from "react-native"
 import { createStackNavigator } from "@react-navigation/stack"
 import HomeScreen from "../../screens/HomeScreen"
+import DetailScreen from "../../screens/DetailScreen"
 import { signOut } from "../../firebase/config"
 
 const HomeScreenStack = createStackNavigator()
@@ -11,6 +12,21 @@ function HomeScreenNavigator({ user }) {
       <HomeScreenStack.Screen
         name="Home"
         component={HomeScreen}
+        options={{
+          headerRight: () => (
+            <Button
+              onPress={() => {
+                signOut()
+              }}
+              title="Logout"
+              color="#000"
+            />
+          ),
+        }}
+      />
+      <HomeScreenStack.Screen
+        name="Detail"
+        component={DetailScreen}
         options={{
           headerRight: () => (
             <Button
