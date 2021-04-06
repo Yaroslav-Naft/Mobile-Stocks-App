1ee4231039ba5c3f141761ba13a623f946e1df76
ynaftulyev@my.bcit.ca <ynaftulyev@my.bcit.ca>
2021-04-06 09:46:29 -0700

    figuring out how to access the user object

--- src/screens/DetailScreen.js
+++ src/screens/DetailScreen.js
@@ -10,10 +10,10 @@ const DetailScreen = ({ route }) => {
   
   const addStock = async () => {
     try {
-      // const res = await firebase
+      const res = await firebase
       //   .auth()
       //   .createUserWithEmailAndPassword(email, password)
-      // const userId = res.user.uid
+      const userId = res.user.uid
       // // store the user in Firestore
       // const data = {
       //   id: userId,
